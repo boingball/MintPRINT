f76b9c41dbd28b0e70577bed3aa42db853db9245
Darren Banfi <33876110+boingball@users.noreply.github.com>
2026-09-05 00:08:14 +0100

    Bump MintPRINT release to 1.3.2

--- src/MintPrintSettings.c
+++ src/MintPrintSettings.c
@@ -203,7 +203,7 @@ static BOOL mp_copy_file(CONST_STRPTR src, CONST_STRPTR dst);
 
 // Define the USED macro for GCC
 #define USED __attribute__((used))
-#define MINTPRINT_SETTINGS_VERSION "1.3.1"
+#define MINTPRINT_SETTINGS_VERSION "1.3.2"
 #define MINTPRINT_DRIVER_DEST ((CONST_STRPTR)"DEVS:Printers/MintPRINT")
 
 /* MintPrint Settings now ships as a single drawer containing both bundled
@@ -229,7 +229,7 @@ static BOOL mp_read_driver_version(CONST_STRPTR path, struct MPDriverVersion *ou
 
 /* Visible both to AmigaOS's Version command and in the About requester. */
 static const char USED mintprint_version[] =
-    "$VER: MintPrintSettings " MINTPRINT_SETTINGS_VERSION " (03.09.2026)";
+    "$VER: MintPrintSettings " MINTPRINT_SETTINGS_VERSION " (04.09.2026)";
 
 // Simple extension check
 BOOL has_extension(const char *filename, const char *ext) {
